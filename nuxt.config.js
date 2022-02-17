00f86a9fd4f471ac63e19afbd43b9e68832d7c5d
zyn <zhu.yn.m@foxmail.com>
2022-02-17 10:15:26 +0800

    add github action config

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'dadjokes_nuxt',
+    title: 'DadJokes_Nuxt',
     htmlAttrs: {
       lang: 'en'
     },
@@ -37,5 +37,9 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
+  },
+  target: 'static',
+  router: {
+    base: '/dadjokes/'
   }
 }
