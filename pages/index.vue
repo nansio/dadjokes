11345f53d0845b9fcd38ed6e6737f247cc0c1229
zyn <zhu.yn.m@foxmail.com>
2022-03-15 10:08:35 +0800

    update

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <h3>Hello there!</h3>
-    <p>You wanna hear some silly jokes?</p>
+    <p>You wanna hear some corny jokes?</p>
     <p>Go to
       <nuxt-link to="/jokes" style="text-decoration:underline;">JokesPage</nuxt-link>   
       to see what's out there...
