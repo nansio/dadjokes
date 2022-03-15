d42503d59815506d67f0a314fd5e05e795f12183
nansio <nansio@users.noreply.github.com>
2022-03-15 02:09:53 +0000

    deploy: 11345f53d0845b9fcd38ed6e6737f247cc0c1229

--- _nuxt/static/1645082838/jokes/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fdadjokes\u002F_nuxt\u002Fstatic\u002F1645082838",layout:"default",error:a,serverRendered:true,routePath:"\u002Fjokes",config:{_app:{basePath:"\u002Fdadjokes\u002F",assetsPath:"\u002Fdadjokes\u002F_nuxt\u002F",cdnURL:a}}}}(null));
